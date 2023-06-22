8118fda00ed591f18f46f4f56446850b5534cca4
RaniKamna <kamna.a139@gmail.com>
2023-06-22 10:11:25 +0530

    restaurant review route added

--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllRestaurants, createRestaurant, getRestaurantDetails, updateRestaurant, deleteRestaurant } = require("../controllers/restaurantController");
+const { getAllRestaurants, createRestaurant, getRestaurantDetails, updateRestaurant, deleteRestaurant, getRestaurantReview, createRestaurantReview } = require("../controllers/restaurantController");
 const { authorizedRoles } = require("../middleware/auth");
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route('/restaurants').get(authorizedRoles('admin'),getAllRestaurants);
 router.route('/restaurant/new').post(createRestaurant);
 router.route('/restaurant/:id').get(getRestaurantDetails).put(updateRestaurant).delete(deleteRestaurant);
+router.route('/restaurant/reviews').get(getRestaurantReview);
+router.route('/restaurant/review/:id').put(createRestaurantReview);
 
 module.exports = router;
